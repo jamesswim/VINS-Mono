cc4fbe04586444f11a7691972afec9700dcbf902
jamesswim <james384712@gmail.com>
2024-09-04 16:33:56 +0800

    Ensure rviz command is available and set ROS environment

--- docker/run.sh
+++ docker/run.sh
@@ -25,6 +25,10 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+
+source /opt/ros/noetic/setup.bash  # 新增: 確保設置ROS環境
+source /root/catkin_ws/devel/setup.bash  # 新增: 確保設置工作空間環境
+
 roscore &
 ROSCORE_PID=$!
 sleep 3
@@ -41,7 +45,7 @@ docker run \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v ${VINS_MONO_DIR}:/root/catkin_ws/src/VINS-Mono/ \
-  ros:vins-mono \
+  vins-mono \
   /bin/bash -c "cd /root/catkin_ws/; \
   source /opt/ros/noetic/setup.bash; \
   catkin config --env-cache --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE=Release; \
